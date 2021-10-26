17367428fd1fd49b582fa2f8f9c68af8a815209f
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-10-26 19:44:59 -0400

    local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 
 // color hour based on time of day.  Gray for past, Red for present, Green for future
 var hour = moment().format("H");
+
 for( i=0; i<24; i++ ) {
     var y = "#"+ i;
     
@@ -19,7 +20,39 @@ for( i=0; i<24; i++ ) {
     } else{};   
 };
 
-// local storage
-$('#test').html("Test");
-localStorage.content = $('#test').html();
-$('#test').html(localStorage.content);
+
+
+
+// get tasks from local storage
+var loadTasks = function() {
+    tasks7 = JSON.parse(localStorage.getItem("tasks7"));
+    tasks8 = JSON.parse(localStorage.getItem("tasks8"));
+  
+}
+// set tasks to local storage on button click
+$(".saveBtn").click(function() {
+    
+    var tasks7 = $("#7").val();
+
+    localStorage.setItem("tasks7", (tasks7));
+    
+  });
+
+// pull from local storage
+
+$('#7').html(localStorage.tasks7);
+
+// set tasks to local storage on button click
+$(".saveBtn").click(function() {
+    
+    var tasks8 = $("#8").val();
+
+    localStorage.setItem("tasks8", (tasks8));
+    
+  });
+
+// pull from local storage
+
+$('#8').html(localStorage.tasks8);
+
+
