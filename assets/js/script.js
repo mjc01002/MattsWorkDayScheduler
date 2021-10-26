ac2383dbcca21170123f63a7ae85e9f62f876f7d
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-10-26 18:46:57 -0400

    Set row to change based on hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,25 @@
-var str = $("#7AMTask").val();
- 
+
+// Get Current Date
+$(document).ready(function(){
+    var date = moment().format("MM/DD/YYYY, h:mm a");
+    $("#currentDay").append(date);
+    });
+
+// color hour based on time of day.  Gray for past, Red for present, Green for future
+var hour = moment().format("H");
+for( i=0; i<24; i++ ) {
+    var y = "#"+ i;
+    
+    if (i < hour){
+        $(y).addClass("past");
+    } else if(i == hour) {
+        $(y).addClass("present");
+    } else if( i > hour){
+        $(y).addClass("future");
+    } else{};   
+};
+
+// local storage
+$('#test').html("Test");
+localStorage.content = $('#test').html();
+$('#test').html(localStorage.content);
