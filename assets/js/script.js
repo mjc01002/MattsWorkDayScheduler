d0e0aeb4a665ffe8a775cb762ce2df2dfec7ded2
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-10-26 20:31:10 -0400

    local storage complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,36 +23,34 @@ for( i=0; i<24; i++ ) {
 
 
 
-// get tasks from local storage
-var loadTasks = function() {
-    tasks7 = JSON.parse(localStorage.getItem("tasks7"));
-    tasks8 = JSON.parse(localStorage.getItem("tasks8"));
-  
-}
+
 // set tasks to local storage on button click
-$(".saveBtn").click(function() {
-    
-    var tasks7 = $("#7").val();
 
-    localStorage.setItem("tasks7", (tasks7));
+  $(".saveBtn").click(function() {
+    for( i=7; i<20; i++ ) {
+        var y = "#"+ i;
+  
+        var tasksa = "tasks" + i;
+        tasksa = $(y).val();
+  
+    localStorage.setItem("tasks" + i, (tasksa));
     
-  });
-
+  }});
 // pull from local storage
 
 $('#7').html(localStorage.tasks7);
-
-// set tasks to local storage on button click
-$(".saveBtn").click(function() {
-    
-    var tasks8 = $("#8").val();
-
-    localStorage.setItem("tasks8", (tasks8));
-    
-  });
-
-// pull from local storage
-
 $('#8').html(localStorage.tasks8);
+$('#9').html(localStorage.tasks9);
+$('#10').html(localStorage.tasks10);
+$('#11').html(localStorage.tasks11);
+$('#12').html(localStorage.tasks12);
+$('#13').html(localStorage.tasks13);
+$('#14').html(localStorage.tasks14);
+$('#15').html(localStorage.tasks15);
+$('#16').html(localStorage.tasks16);
+$('#17').html(localStorage.tasks17);
+$('#18').html(localStorage.tasks18);
+$('#19').html(localStorage.tasks19);
 
 
+  
